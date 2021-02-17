c49d973f229aa2813345744c8c6ac0f846c81680
aBian0609 <glareboy@outlook.com>
2021-02-17 13:00:50 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Header class="header" msg="Welcome to Your Vue.js App [Header components]"/>
+  <div class="artice">
+    <Left class="artice-left" msg="Welcome to Your Vue.js App [Left components]"/>
+    <Right class="artice-right" msg="Welcome to Your Vue.js App [Right components]"/>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import Left from './components/Left.vue'
+import Right from './components/Right.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Left,
+    Right,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.artice{
+    background-color: #f1f1f1;
+    display: flex;
+    justify-content: center;
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+    background-color: #f1f1f1;
+    display: flex;
+    justify-content: center;
+}
+</style>
